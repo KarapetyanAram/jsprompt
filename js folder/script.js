21c2991405f371638002ac63c1178db504f37f30
KarapetyanAram <aramkarapetyan222@mail.ru>
2022-02-12 16:49:39 +0800

    my first commit

---
+++ js folder/script.js
@@ -0,0 +1,13 @@
+
+let x = prompt("how old are you?");
+let y = prompt("what is your name?")
+let b = prompt("what is the capital of armenia?")
+let i = prompt("how many people  is there?")
+
+let arr =[];
+arr[0] = x;
+arr[1] = y;
+arr[2] = b;
+arr[3] = i;
+
+console.log(arr);
